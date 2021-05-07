a199f7b17d528b3593192c8d7f93b3276f758612
Isaac <siphron0@gmail.com>
2021-05-08 01:09:46 +0200

    [ADD] More User test

--- src/test/java/model/UserTest.java
+++ src/test/java/model/UserTest.java
@@ -16,12 +16,37 @@ public class UserTest extends TestCase {
     public void testIsValid() throws Exception{
         assertTrue(user.isValid());
     }
+    //TODO email
 
     @Test
-    public void testFirstNameLengthIsLess20() throws Exception{
+    public void testFirstNameExist() throws Exception{
+        assertNotNull(user.firstname);
+    }
+
+    @Test
+    public void testLastNameExist() throws Exception{
+        assertNotNull(user.lastname);
+    }
+
+    @Test
+    public void testFirstNameLengthIsLessThan20() throws Exception{
         assertTrue(user.firstname.length()<20);
     }
 
+    @Test
+    public void testLastNameLengthIsLessThan20() throws Exception{
+        assertTrue(user.lastname.length()<20);
+    }
 
+    @Test
+    public void testUserIsOlderThan13YearsOld() throws Exception{
+        assertTrue(user.birthdate.isBefore(LocalDate.now().minusYears(13)));
+    }
+
+    @Test
+    public void testUserIsYoungerThan13YearsOld() throws Exception{
+        user.setBirthdate(LocalDate.now().minusYears(10));
+        assertFalse(user.birthdate.isBefore(LocalDate.now().minusYears(13)));
+    }
 
 }
