c2199dfcaf35815b90ccb3b0e4461c85a9ce002e
christopheylp <christopheyalap957@gmail.com>
2021-05-08 21:34:35 +0200

    Updated test for content limit size

--- src/test/java/model/ItemTest.java
+++ src/test/java/model/ItemTest.java
@@ -35,6 +35,7 @@ public class ItemTest extends TestCase {
         String str = "";
         for(int i=0; i<1200;i++) str += "a";
         Item itemTest = new Item("item1", str);
+        assertTrue(str.length() > 1000);
         assertTrue(itemTest.content.length() <= 1000);
     }
 }
